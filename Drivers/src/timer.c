21fee539ef9744de1ecbb29c1200c716d76c6eee
gaurav phadke <grvgaurav03@gmail.com>
2021-10-08 10:19:07 +0530

    Adjusted Prscalar values

--- Drivers/src/timer.c
+++ Drivers/src/timer.c
@@ -17,9 +17,9 @@
  *4. Set the PWR bit[15] to 1
  *5. Set the ICEN bit[9], PRFTEN Bit[8] , DCEN bit[10] to 1 and Latency to 3
  *6. Set respective prescalar of PLL_CFGR register
- *7. PLLM - /25 - PLL_CFGR bit[0] set to 25
- *8. PLLN - x168 - PLL_CFGR bit[6] set to 168
- *9. PLLP - /2	 - PLL_CFGR bit[16] set to 0
+ *7. PLLM - /25 - RCC_PLLCFGR bit[0] set to 25
+ *8. PLLN - x168 - RCC_PLLCFGR bit[6] set to 168
+ *9. PLLP - /2	 - RCC_PLLCFGR bit[16] set to 0
  *10. Configure the AHB APB1 and APB2 buss prescalar
  *11. Configuration in RCC_CFGR - APB1 bit[10] to 100 for /2 prescalar
  *11. configuration in RCC_CFGR - APB2 bit[13] to 100 for /2 prescalar
@@ -48,15 +48,15 @@ void system_clk()
 	RCC->RCC_PLLCFGR |= (1<<22);
 	/*
 	 * setting PLLM according to the clock in CUBEmx
-	 * according to the prescalar /25
+	 * according to the prescalar /8 - Push value 8
 	 */
-	RCC->RCC_PLLCFGR |= (25 << 0);
+	RCC->RCC_PLLCFGR |= (8 << 0);
 	/*
 	 *  PLLN settings
-	 *  /168 IN PUSHED
+	 *  /84 IN PUSHED
 	 *
 	 */
-	RCC->RCC_PLLCFGR |= (168<<6);
+	RCC->RCC_PLLCFGR |= (84<<6);
 	/*
 	 *PLLP SETTING
 	 *PLLP /2 IS PUSHED
@@ -65,9 +65,10 @@ void system_clk()
 	/*
 	 *  Setting PPRE1 WITH 100 AS /2
 	 */
-	RCC->RCC_CFGR |= (4<<10); // 100 IS 4
-	RCC->RCC_CFGR |= (4<<13); // 100 IS 4
-	RCC->RCC_CFGR &=(1<<4);  // 000 IS /1
+	RCC->RCC_CFGR |=(4<<10); // 100 IS 2 APB1
+	RCC->RCC_CFGR &=(1<<13); // 000 IS 1 APB2
+	RCC->RCC_CFGR &=(1<<4);  // 000 IS /1 AHB
+
 	// TURN ON PLL
 	RCC->RCC_CR |= (1<<24);
 	while(RCC->RCC_CR &(1<<25)); // WAIT TILL PLLRDY BIT IS 1
