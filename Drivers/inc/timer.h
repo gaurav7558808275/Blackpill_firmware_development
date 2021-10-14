610c3bd3027aff13936ae50a88a076e7811e0622
gaurav phadke <grvgaurav03@gmail.com>
2021-10-14 11:53:31 +0530

    RCC clock update

--- Drivers/inc/timer.h
+++ Drivers/inc/timer.h
@@ -8,9 +8,20 @@
 #ifndef DRIVERS_INC_TIMER_H_
 #define DRIVERS_INC_TIMER_H_
 
+#include "blackpill.h"
 
 
+/*
+ * We will create funtions for delay function
+ *  like microsecond delay
+ *  millisecond delay
+ *  and secod delay.
+ */
 
+
+void us_delay();
+void ms_delay();
+void s_delay();
 
 
 
