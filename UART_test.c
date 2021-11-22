8725da41f4de5fb9921b7a3850544e4d63f72e68
gaurav phadke <grvgaurav03@gmail.com>
2021-11-22 12:54:10 +0530

    UART api implementation

--- UART_test.c
+++ UART_test.c
@@ -9,9 +9,11 @@
 #include "string.h"
 #include "blackpill.h"
 #include "UART.h"
+#include "gpio.h"
 
 // handle init
 USART_Handle_t Uart;
+GPIOx_Handle GPIO;
 char msg[1024] = "Hello, the rest is ready";
 // creation of UART init
 
@@ -24,19 +26,32 @@ void uart_init(void){
 		Uart.USART_Config.USART_paritybit = USART_PARITY_DISABLE;
 		Uart.USART_Config.USART_wordlength = USART_WORDLEN_8BITS;
 	}
+void gpio_init(void){
+	GPIO.pGPIOx = GPIOD;
+	GPIO.GPIO_Pin_config.GPIO_PinMode=GPIO_MODE_OUT;
+	GPIO.GPIO_Pin_config.GPIO_PinNumber = GPIO_PIN_12;
+	GPIO.GPIO_Pin_config.GPIO_PinOutputType = GPIO_MODE_OUT_OD;
+	GPIO.GPIO_Pin_config.GPIO_PinSpeed = GPIO_SPEED_FAST;
+	GPIO.GPIO_Pin_config.GPIO_PinPuPdcontrol = GPIO_PORT_N_PP;
+}
 
 
 
 int main(void){
 
 
-	// Main UART_INIT()
+	// config UART_INIT()
 	uart_init();
+	// main uart_init
 	USART_Init(&Uart);
-
-
-
+	//config gpio_init()
+	gpio_init();
+	//main gpio_init()
+	GPIO_Init(&GPIO);
+	//UART start
+	USART_PeripheralControl(Uart.pUSARTx,ENABLE);
 	while(1){
+
 
 	}
 
