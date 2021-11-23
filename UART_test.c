487eccd7cd2ec289aa513eb253e9ee4b39d29183
gaurav phadke <grvgaurav03@gmail.com>
2021-11-23 10:14:49 +0530

    application file modified

--- UART_test.c
+++ UART_test.c
@@ -15,7 +15,7 @@
 
 // handle init
 USART_Handle_t Uart;
-GPIOx_Handle GPIO;
+GPIOx_Handle GPIO_led;
 char msg[1024] = "Hello, the UART is ready";
 // creation of UART init
 
@@ -27,14 +27,16 @@ void uart_init(void){
 		Uart.USART_Config.USART_no_ofstopbits = USART_STOPBITS_1;
 		Uart.USART_Config.USART_paritybit = USART_PARITY_DISABLE;
 		Uart.USART_Config.USART_wordlength = USART_WORDLEN_8BITS;
+		USART_Init(&Uart);
 	}
 void gpio_init(void){
-	GPIO.pGPIOx = GPIOD;
-	GPIO.GPIO_Pin_config.GPIO_PinMode=GPIO_MODE_OUT;
-	GPIO.GPIO_Pin_config.GPIO_PinNumber = GPIO_PIN_12;
-	GPIO.GPIO_Pin_config.GPIO_PinOutputType = GPIO_MODE_OUT_OD;
-	GPIO.GPIO_Pin_config.GPIO_PinSpeed = GPIO_SPEED_FAST;
-	GPIO.GPIO_Pin_config.GPIO_PinPuPdcontrol = GPIO_PORT_N_PP;
+	GPIO_led.pGPIOx = GPIOD;
+	GPIO_led.GPIO_Pin_config.GPIO_PinMode=GPIO_MODE_OUT;
+	GPIO_led.GPIO_Pin_config.GPIO_PinNumber = GPIO_PIN_12;
+	GPIO_led.GPIO_Pin_config.GPIO_PinOutputType = GPIO_MODE_OUT_OD;
+	GPIO_led.GPIO_Pin_config.GPIO_PinSpeed = GPIO_SPEED_FAST;
+	GPIO_led.GPIO_Pin_config.GPIO_PinPuPdcontrol = GPIO_PORT_N_PP;
+	GPIO_Init(&GPIO_led);
 }
 
 
@@ -44,17 +46,14 @@ int main(void){
 
 	// config UART_INIT()
 	uart_init();
-	// main uart_init
-	USART_Init(&Uart);
 	//config gpio_init()
 	gpio_init();
-	//main gpio_init()
-	GPIO_Init(&GPIO);
+
 	//UART start
 	USART_PeripheralControl(Uart.pUSARTx,ENABLE);
 	while(1){
 			while(!GPIO_Read_Pin(GPIOD, GPIO_PIN_12));
-			s_delay(1);
+			ms_delay(1);
 			USART_SendData(&Uart, (uint8_t*)msg,strlen(msg));
 
 	}
